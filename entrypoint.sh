249f50e5718623067fd96946b26635822ad0363b
Peter Baumgartner <pete@lincolnloop.com>
2021-04-05 16:55:55 -0600

    Better handling of files generated during build

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,18 +18,11 @@ jq -r .source.buildspec < build.json > buildspec.yml
 yj < buildspec.yml | jq -r .phases.install.commands > script.sh
 yj < buildspec.yml | jq -r .phases.pre_build.commands >> script.sh
 yj < buildspec.yml | jq -r .phases.build.commands >> script.sh
+rm build.json buildspec.yml
 # run build script
 echo "::group::Running build process"
 bash script.sh
 echo "::endgroup::"
 echo "Fixing file permissions"
-chown 1001 build.json
-echo "  * build.json"
-chown 1001 buildspec.yml
-echo "  * buildspec.yml"
-for FILE in $(yj < buildspec.yml | jq -r '.artifacts.files[]'); do
-  echo "  * $FILE"
-  test -f "$FILE" && chown 1001 "$FILE" || echo "::warning::$FILE does not exist"
-
-done
+find . -user 0 -maxdepth 1 -type f -exec chown 1001 {} +
 echo "::set-output name=docker_image::$DOCKER_REPO:$GITHUB_SHA"
